ea338603087a1d11809916388198880bf7f80786
Diego <daer314@hotmail.com>
2023-05-08 16:24:30 -0500

    First commit

---
+++ src/components/Videos.jsx
@@ -0,0 +1,21 @@
+import { VideoCard, ChannelCard } from './index'
+
+import { Stack, Box } from "@mui/material"
+
+const Videos = ({ videos, direction }) => {
+
+  if(!videos?.length) return 'Loading...';
+
+  return (
+    <Stack direction={direction || 'row'} flexWrap='wrap' justifyContent='start' gap={2} >
+      { videos.map((item, idx) => (
+        <Box key={idx}>
+          { item.id.videoId && <VideoCard video={item}/>}
+          { item.id.channelId && <ChannelCard channelDetail={item}/>}
+        </Box>
+      ))}
+    </Stack>
+  )
+}
+
+export default Videos
